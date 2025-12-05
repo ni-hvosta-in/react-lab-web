32798252e4552b202c2eacf2d32ae77206ea553f
Alexandr <alpinefuchs@gmail.com>
2025-12-05 14:41:01 +0300

    startPage

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+
+export default function AuthForm() {
+
+    const [typeOfAuth, setTypeOfAuth] = useState("");
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    const [errorTextForUser, setErrorText] = useState("");
+
+    useEffect(() =>{
+        setErrorText("");
+    }, [typeOfAuth, login, password, repeatPassword]);
+
+    const AuthType = {
+        SIGN_IN: "Sign in",
+        SIGN_UP: "Sign up"
+    } as const;
+
+    type AuthType = typeof AuthType[keyof typeof AuthType];
+
+    async function postForm(event: React.FormEvent<HTMLFormElement>){
+        
+        event.preventDefault();
+        
+        if (typeOfAuth === AuthType.SIGN_UP && password !== repeatPassword) {
+            setErrorText("Пароли не совпадают");
+            return;
+        }
+
+        setErrorText("");
+
+        try{
+
+            let response = await fetch('http://localhost:8080/auth', 
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        login: login,
+                        password: password,
+                        typeOfAuth: typeOfAuth
+                    })
+                });
+                
+            const data = await response.json();
+            if (response.ok) {
+                console.log(data.message);  
+                sessionStorage.setItem('token', data.token);
+                setErrorText("");
+            } else {
+                setErrorText(data.message);
+            }
+            
+        } catch (error) {
+            console.error('Error:', error);
+            alert("Сервер недоступен. Попробуйте позже.");
+        }
+    }
+
+    const renderContent = () => {
+        if (typeOfAuth == "") {
+            return (
+                    <div className="authForm">
+                        <button onClick={() => setTypeOfAuth(AuthType.SIGN_IN)}>{AuthType.SIGN_IN}</button>
+                        <button onClick={() => setTypeOfAuth(AuthType.SIGN_UP)}>{AuthType.SIGN_UP}</button>
+                    </div>
+                    );
+        } else {
+            
+            return (
+
+                <form className="authForm" onSubmit={postForm}> 
+                    <input type="text" placeholder="Введите логин" onChange={(event) => {setLogin(event.target.value)}}/>
+                    <input type="password" placeholder="Введите пароль" onChange={(event) => {setPassword(event.target.value)}}/>
+                    {typeOfAuth == AuthType.SIGN_UP && <input type="password" placeholder="Повторите пароль" onChange={(event) => {setRepeatPassword(event.target.value)}}/>}
+
+                    <button type = "submit">{typeOfAuth == AuthType.SIGN_IN ? AuthType.SIGN_IN : AuthType.SIGN_UP}</button>
+                    <button onClick={() => setTypeOfAuth("")} type="button">Back</button>
+                    {errorTextForUser}
+                    
+                </form>
+                
+                );
+
+        }
+    }
+
+    return (
+        <div>
+           {renderContent()}
+        </div>
+    );
+}
